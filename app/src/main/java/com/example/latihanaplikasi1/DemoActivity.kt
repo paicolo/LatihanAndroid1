2c2812c1f736c95e2454cd60c29f82f575339e9a
Luthfii <luthfinurfadhillah02@gmail.com>
2022-10-25 16:37:07 +0700

    tampilan 2

--- app/src/main/java/com/example/latihanaplikasi1/DemoActivity.kt
+++ app/src/main/java/com/example/latihanaplikasi1/DemoActivity.kt
@@ -3,13 +3,17 @@ package com.example.latihanaplikasi1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -17,7 +21,11 @@ class DemoActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent() {
-            Column(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+                Image(painter = painterResource(id = R.drawable.),
+                    contentDescription = "Gambar")
                 Surface(
                     modifier = Modifier.fillMaxWidth(),
                     color = Color.Red
@@ -26,7 +34,8 @@ class DemoActivity : ComponentActivity(){
                         text = "Mantappu",
                         fontSize = 24.sp,
                         modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 4.dp),
-                        color = Color.Magenta
+                        color = Color.Magenta,
+                        textAlign = TextAlign.Center
                     )
                 }
 
@@ -37,7 +46,8 @@ class DemoActivity : ComponentActivity(){
                     Text(
                         text = "Jiwa",
                         modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 4.dp),
-                        color = Color.Black
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
                     )
                 }
             }
